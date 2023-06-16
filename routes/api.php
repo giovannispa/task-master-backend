6f70dc3cb16707f16e10578a905a6353bceeb4d0
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-06-15 21:02:21 -0300

    Feature // Implementei as rotas inicias do sistema para testes.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->json(['message' => 'Hello World!']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/me', [AuthController::class, 'me'])->name('me');
+    Route::get('/', function () {
+        return response()->json(['message' => 'Hello World!']);
+    });
 });
