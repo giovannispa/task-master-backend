9176c437c70219b20a8823f2554e638254bc6316
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-07-24 23:01:06 -0300

    Feature // Documentei o user repository.

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -7,6 +7,11 @@ use App\Models\User;
 
 class UserRepository extends BaseRepository implements UserRepositoryInterface
 {
+    /**
+     * Construtor.
+     *
+     * @param User $model
+     */
     public function __construct(User $model)
     {
         parent::__construct($model);
