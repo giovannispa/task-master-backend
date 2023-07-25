13e6037d7a3e0b2210de67b4b760c9fae7ede02c
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-07-24 23:00:16 -0300

    Feature // Implementei o relacionamento 1 para 1 de usuário com categoria do mesmo.

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Função que retorna a categorias do usuário.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
