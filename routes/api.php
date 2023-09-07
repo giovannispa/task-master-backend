c168c510adfca1a05a923a6902e60bd1870d72f6
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-09-07 20:34:01 -0300

    Feature // Abastrai as rotas de usuário e categoria com apiResource

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\{
+    UserController,
+    CategoryController
+};
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login'])->name('login');
@@ -10,15 +12,9 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::post('/me', [AuthController::class, 'me'])->name('me');
-    Route::get('/', function () {
-        return response()->json(['message' => 'Hello World!']);
-    });
+
+    Route::apiResource('/users', UserController::class);
+
 });
 
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/user/create', [UserController::class, 'create']);
-Route::post('/user/store', [UserController::class, 'store']);
-Route::get('/user/{id}/show', [UserController::class, 'show']);
-Route::get('/user/{id}/edit', [UserController::class, 'edit']);
-Route::put('/user/{id}/update', [UserController::class, 'update']);
-Route::delete('/user/{id}/delete', [UserController::class, 'destroy']);
+Route::apiResource('/categories', CategoryController::class);
