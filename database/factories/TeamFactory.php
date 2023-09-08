71f4b0086e9299da7cbc4cc11ce6e0e0125be405
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-09-08 19:32:30 -0300

    Testes // Criei a factory de equipe.

---
+++ database/factories/TeamFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Team>
+ */
+class TeamFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->company,
+            'active_projects' => rand(1,10),
+            'worked_projects' => rand(1,10),
+            'tasks_performed' => rand(1,100)
+        ];
+    }
+}
