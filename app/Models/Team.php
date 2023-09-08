ca5f81e969490fd7354530ce8f18840b62f8c4fb
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-09-08 19:34:25 -0300

    Feature // Criei e implementei a model de equipe.

---
+++ app/Models/Team.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Team extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'leader_id',
+        'name',
+        'active_projects',
+        'worked_projects',
+        'tasks_performed',
+    ];
+
+    /**
+     * Função que retorna os usuários da equipe.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
