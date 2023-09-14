c30188feb470e8d7dd94e32f2201c20fa618839f
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-09-13 22:53:43 -0300

    Feature // Criei o service de projetos.

---
+++ app/Services/ProjectService.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Services;
+
+use App\Interfaces\ProjectRepositoryInterface;
+
+class ProjectService
+{
+    /**
+     * @var ProjectRepositoryInterface
+     */
+    private ProjectRepositoryInterface $repository;
+
+    /**
+     * Construtor.
+     *
+     * @param ProjectRepositoryInterface $projectRepository
+     */
+    public function __construct(ProjectRepositoryInterface $projectRepository)
+    {
+        $this->repository = $projectRepository;
+    }
+
+    /**
+     * Encontra primeiro projeto que corresponda a um valor específico de uma coluna.
+     *
+     * @param string $column
+     * @param mixed $value
+     * @return object
+     */
+    public function findWhereFirst(string $column, mixed $value): object
+    {
+        return $this->repository->findWhereFirst($column, $value);
+    }
+
+    /**
+     * Retorna todos os projetos.
+     *
+     * @return array
+     */
+    public function all(): array
+    {
+        return $this->repository->all();
+    }
+
+    /**
+     * Cria um novo projeto.
+     *
+     * @param array $data
+     * @return object
+     */
+    public function create(array $data): object
+    {
+        return $this->repository->create($data);
+    }
+
+    /**
+     * Encontra um projeto pelo ID.
+     *
+     * @param int $id
+     * @return object
+     */
+    public function find(int $id): object
+    {
+        return $this->repository->find($id);
+    }
+
+    /**
+     * Deleta uma equipe pelo ID.
+     *
+     * @param string $id
+     * @return bool
+     */
+    public function delete(string $id): bool
+    {
+        return $this->repository->delete($id);
+    }
+
+    /**
+     * Atualiza um projeto pelo ID.
+     *
+     * @param int $id
+     * @param array $data
+     * @return object
+     */
+    public function update(int $id, array $data): object
+    {
+        return $this->repository->update($id, $data);
+    }
+
+    /**
+     * Faz o relacionamento entre projeto e equipe.
+     *
+     * @param string $relation
+     * @param int $primary_id
+     * @param int $foreign_id
+     * @return object|null
+     */
+    public function attach(string $relation, int $primary_id, int $foreign_id): ?object
+    {
+        return $this->repository->attach($relation, $primary_id, $foreign_id);
+    }
+
+    /**
+     * Remove o relacionamento entre projeto e equipe.
+     *
+     * @param string $relation
+     * @param int $primary_id
+     * @param int $foreign_id
+     * @return object|null
+     */
+    public function detach(string $relation, int $primary_id, int $foreign_id): ?object
+    {
+        return $this->repository->detach($relation, $primary_id, $foreign_id);
+    }
+}
