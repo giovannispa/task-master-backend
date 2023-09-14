5597ea407251f3b569e3a53595565cfd6c11b9c7
Giovanni Sertorio <giovanni.spadoni10@hotmail.com>
2023-09-13 22:51:27 -0300

    Hotfix // Removi o id de time desnecessário da tabela de projetos.

--- database/migrations/2023_05_24_221929_create_projects_table.php
+++ database/migrations/2023_05_24_221929_create_projects_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id()->comment("Id do projeto");
             $table->foreignId('status_id')->comment("Id do status do projeto")->constrained();
-            $table->foreignId('team_id')->comment("Id do time do projeto")->constrained();
             $table->string('name')->comment("Nome do projeto");
             $table->text('description')->nullable()->comment("Descrição do projeto");
             $table->date('start_date')->nullable()->comment("Data de início do projeto");
